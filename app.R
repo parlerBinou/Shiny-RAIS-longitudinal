2d13ae6c2097b88b40e63db4bd0c0fd6ac9f3e36
GenerarlYangChicken <54256872+chaoyangzhengnash@users.noreply.github.com>
2021-04-28 11:30:41 -0400

    Add files via upload

---
+++ app.R
@@ -0,0 +1,73 @@
+library(shiny)
+library(plotly)
+library(tidyverse)
+#library(reshape2)
+#library(shinyjs)
+#library(shinythemes)
+library(shinydashboard)
+library(plyr)
+library(circlize)
+#library(Cairo)
+library(shinyWidgets)
+options(shiny.usecairo=T)
+
+source("R/download_data.R")
+source("R/module_pathway.R")
+source("R/module_mobility_matrix.R")
+source("R/module_mobility_measure.R")
+
+dictionary <- read.csv('dictionary/dict_main.csv',encoding = "utf-8")
+translation <- dlply(dictionary ,.(key), function(s) key = as.list(s))
+
+language <- "en"
+
+tr <- function(text){ 
+  ls <-lapply(text,function(s) translation[[s]][[language]])
+  return(ls[[1]])
+}
+
+ui <- bootstrapPage(
+  
+  tags$head(
+    tags$script(
+      '$(document).on("shiny:connected", function(e) {
+              Shiny.onInputChange("innerSize", Math.min(window.innerWidth * 0.65, window.innerHeight * 0.85));
+            });
+           $(window).resize(function(e) {
+              Shiny.onInputChange("innerSize", Math.min(window.innerWidth * 0.65, window.innerHeight * 0.85));
+            });
+        ')),
+  
+  navbarPage(
+    
+    title=NULL,
+    
+    tabPanel(
+      tr("title_pathway"),
+      pathway_ui("pathway")
+    ), 
+    
+    tabPanel(
+      tr("title_mob_measures"),
+      mob_measure_ui("mob_measure")
+    ),
+    tabPanel(
+      tr("title_mob_matrix"),
+      mob_matrix_ui("mob_matrix")
+    )
+    
+  ) # navbar page
+) # bootstrap page
+
+
+server <- function(input, output, session) {
+  
+  pathway_server("pathway", language)
+  
+  mob_measure_server("mob_measure", language)
+  
+  mob_matrix_server("mob_matrix", language, reactive(input$innerSize))
+  
+}
+
+shinyApp(ui, server)
